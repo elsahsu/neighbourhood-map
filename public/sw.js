ce3192ae3da87e55cf63e66425ddbc7888382c1d
Elsa Hsu <elsanfhsu@gmail.com>
2018-08-06 00:44:36 +0300

    Save data to cache to allow offline use

--- public/sw.js
+++ public/sw.js
@@ -1,13 +1,29 @@
-// This service worker file is effectively a 'no-op' that will reset any
-// previous service worker registered for the same host:port combination.
-// In the production build, this file is replaced with an actual service worker
-// file that will precache your site's local assets.
-// See https://github.com/facebookincubator/create-react-app/issues/2272#issuecomment-302832432
+const CACHE_NAME = 'tampere-cache-v1';
 
-self.addEventListener('install', () => self.skipWaiting());
+
+self.addEventListener('install', event => {
+  console.log("Install: Saving static files to cache");
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(function(cache) {
+      console.log("Caching...");
+      return cache.addAll([
+        '/',
+        '/sw.js',
+        '/util.js',
+        '/common.js',
+        '/manifest.json',
+        '/static/js/bundle.js'
+      ])
+      .catch(function(error) {
+        console.log('Failed to add files to cache');
+        console.log(error);
+      });
+    })
+  );
+});
 
 self.addEventListener('activate', () => {
-  console.log('SW Tadaa!');
+  console.log("Activating");
   self.clients.matchAll({ type: 'window' }).then(windowClients => {
     for (let windowClient of windowClients) {
       // Force open pages to refresh, so that they have a chance to load the
@@ -15,4 +31,25 @@ self.addEventListener('activate', () => {
       windowClient.navigate(windowClient.url);
     }
   });
+});
+
+self.addEventListener('fetch', event => {
+  console.log('Fetching:', event.request);
+  // if (event.request.url.startsWith(self.location.origin)) {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      console.log('Fetching from cache:', event.request);
+      if (response) {
+        console.log('Serving request from cache');
+        return response;
+      }
+      return fetch(event.request).then(response => {
+        return caches.open(CACHE_NAME).then(cache => {
+          console.log('Request not found in cache, caching now');
+          cache.put(event.request.url, response.clone());
+          return response;
+        });
+      })
+    })
+  );
 });
